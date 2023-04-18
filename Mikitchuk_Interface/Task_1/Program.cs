5a3f79f235f8e7b10f6855838a8245742c7846a2
Viacheslav Mikitchuk <89093010+FenikssFame@users.noreply.github.com>
2023-04-18 11:01:57 +0300

    18/04/23 create program

---
+++ Mikitchuk_Interface/Task_1/Program.cs
@@ -0,0 +1,101 @@
+﻿using Task_1;
+
+namespace Task_1
+{
+    // Пара интерфейсов, содержащих объявления одноимённых методов
+    // с одной и той же сигнатурой.
+    interface Ix
+    {
+        int IxF0(int num);
+        int IxF1();
+    }
+    interface Iy
+    {
+        int F0(int num);
+        int F1();
+    }
+    interface Iz
+    {
+        int F0(int num);
+        int F1();
+    }
+    class TestClass : Ix, Iy, Iz
+    {
+        public int wValue;
+        public TestClass()
+        {
+            wValue = 12;
+        }
+        public TestClass(int value)
+        {
+            wValue = value;
+        }
+        // Реализация неявная и однозначная
+        public int IxF0(int num)
+        {
+            wValue = 7 * num;
+            return wValue;
+        }
+        public int IxF1()
+        {
+            wValue = 7 * wValue;
+            return wValue;
+        }
+        // Неявная неоднозначная реализация интерфейсов
+        public int F0(int num)
+        {
+            wValue = num * 8;
+            return wValue;
+        }
+        public int F1()
+        {
+            wValue = wValue * 8;
+            return wValue;
+        }
+        // Явная непосредственная реализация интерфейса
+        int Iz.F0(int num)
+        {
+            wValue = 6 + num;
+            return wValue;
+        }
+        int Iz.F1()
+        {
+            wValue = 6 + wValue;
+            return wValue;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для функции F1: ");
+            int numF1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите число для функции F0: ");
+            int numF0 = int.Parse(Console.ReadLine());
+            TestClass tc = new TestClass();
+            TestClass tcNum = new TestClass(numF1);
+            // Неявная неоднозначная реализация интерфейсов Iy и Iz
+            Console.WriteLine($"Неявная неоднозначная реализация интерфейсов\n" +
+                $"Iy и Iz = {tc.F0(numF0)}\n" +
+                $"Iy и Iz = {tcNum.F1()}");
+            // Явное приведение к типу интерфейса
+            Console.WriteLine($"Явное приведение к типу интерфейса\n" +
+                $"Iy = {(tc as Iy).F0(numF0)}\n" +
+                $"Iy = {(tcNum as Iy).F1()}\n" +
+                $"Iz = {(tc as Iz).F0(numF0)} \n" +
+                $"Iz = {(tcNum as Iz).F1()}");
+            // Ссылки различных типов интерфейсов
+            Console.WriteLine("Ссылки различных типов интерфейсов");
+            Ix ix = tcNum;
+            Console.WriteLine($"IxF0 = {ix.IxF0(numF0)}\n" +
+                $"IxF1 = {ix.IxF1()}");
+            Iy iy = tcNum;
+            Console.WriteLine($"IyF0 = {iy.F0(numF0)}\n" +
+                $"IyF1 = {iy.F1()}");
+            Iz iz = tcNum;
+            Console.WriteLine($"IxF0 = {iz.F0(numF0)}\n" +
+                $"IxF1 = {iz.F1()}");
+        }
+    }
+}
---
+++ Mikitchuk_Interface/Task_2/Program.cs
@@ -0,0 +1,207 @@
+﻿using System.Threading.Channels;
+
+namespace MainProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Massiv mas = new Massiv();
+                mas.list = mas.GetCreateArray(GetSizeArray());
+                mas.list = mas.GetCreateArray(GetSizeArray());
+                Console.WriteLine($"Перечень массивов");
+                PrintListArray(mas.list);
+
+                Console.WriteLine("Введите номер массива и его значение к которому хотите обратится");
+                Console.Write("Номер массива:");
+                int indexArray = int.Parse(Console.ReadLine());
+                Console.Write("Значение:");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Вы обратились к элементу под индексом: {mas.GetElementIndex(indexArray, number)}");
+
+                Console.Write($"Выберите массивы для операций\n" +
+                    $"Введите номер массива в списке: ");
+                int one = int.Parse(Console.ReadLine());
+                Console.Write("Введите номер массива в списке: ");
+                int two = int.Parse(Console.ReadLine());
+                Console.WriteLine("Сложение массивов");
+                PrintArray(mas.GetResultOperationSum(mas.list[one], mas.list[two]));
+                Console.WriteLine("Вычитание массивов");
+                PrintArray(mas.GetResultOperationSub(mas.list[one], mas.list[two]));
+
+                Console.Write($"Выберите массив\n" +
+                    $"Введите номер массива в списке: ");
+                int[] array = mas.GetArray(mas.list, int.Parse(Console.ReadLine()));
+                Console.Write("Выберите номер элемента массива: ");
+                Console.WriteLine($"Вы выбрали: {mas.GetElement(array, int.Parse(Console.ReadLine()))}");
+                Console.Write($"Из массива: ");
+                PrintArray(array);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static int GetSizeArray()
+        {
+            try
+            {
+                Console.Write("Введите размерность массива: ");
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+        public static void PrintArray(int[] array)
+        {
+            try
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void PrintListArray(List<int[]> list)
+        {
+            try
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    int[] array = list[i];
+                    Console.WriteLine($"{i} массив");
+                    PrintArray(array);
+                }
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Massiv : IComparable
+    {
+        public List<int[]> list;
+        public Massiv()
+        {
+            list = new List<int[]>();
+        }
+        public List<int[]> GetCreateArray(int Length)
+        {
+            try
+            {
+                int[] array = new int[Length];
+                Random R = new Random();
+                for (int i = 0; i < Length; i++)
+                {
+                    array[i] = R.Next(100);
+                }
+                list.Add(array);
+                return list;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return list;
+            }
+        }
+        public int[] GetArray(List<int[]> list, int index)
+        {
+            try
+            {
+                return list[index];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new int[] { 0 };
+            }
+        }
+        public int GetElement(int[] array, int index)
+        {
+            try
+            {
+                return array[index];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+        public int GetElementIndex(int indexArray, int num)
+        {
+            try
+            {
+                int[] array = GetArray(list, indexArray);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == num)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+        public int[] GetResultOperationSum(int[] ferstArray, int[] secondArray)
+        {
+            if (GetBoolEqualLengthArray(ferstArray, secondArray))
+            {
+                int length = ferstArray.Length;
+                int[] resultArray = new int[length];
+                for (int i = 0; i < length; i++)
+                {
+                    resultArray[i] = ferstArray[i] + secondArray[i];
+                }
+                return resultArray;
+            }
+            else
+            {
+                return new int[] { 0 };
+            }
+        }
+        public int[] GetResultOperationSub(int[] ferstArray, int[] secondArray)
+        {
+            if (GetBoolEqualLengthArray(ferstArray, secondArray))
+            {
+                int length = ferstArray.Length;
+                int[] resultArray = new int[length];
+                for (int i = 0; i < length; i++)
+                {
+                    resultArray[i] = ferstArray[i] - secondArray[i];
+                }
+                return resultArray;
+            }
+            else
+            {
+                return new int[] { 0 };
+            }
+        }
+        public bool GetBoolEqualLengthArray(int[] ferstArray, int[] secondArray)
+        {
+            if (ferstArray.Length == secondArray.Length) return true;
+            else return false;
+        }
+        public int CompareTo(object? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
