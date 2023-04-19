8ab4d22f75b44278d62f6b748835a351e244c223
Viacheslav Mikitchuk <89093010+FenikssFame@users.noreply.github.com>
2023-04-19 21:42:12 +0300

    19/04/23 create programs

---
+++ Mikitchuk_LegacyClasses/Task_1/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Task_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Marker markers = new Marker();
+                markers.ProductCost();
+                markers.GetInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    class Product
+    {
+        private int count;
+        private double cost;
+        private double result;
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+
+        }
+        public double Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+
+        }
+        public double Result
+        {
+            get { return result; }
+            set { result = value; }
+
+        }
+        public Product()
+        {
+            this.count = 0;
+            this.cost = 0;
+            this.result = 0;
+            Input();
+        }
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"Кол-во: {count}\n" +
+                $"Цена: {cost}");
+        }
+        public virtual void ProductCost()
+        {
+            result = cost * count;
+        }
+        public void Input()
+        {
+            Console.Write("Введите цену: ");
+            cost = double.Parse(Console.ReadLine());
+            Console.Write("Введите количество: ");
+            count = int.Parse(Console.ReadLine());
+        }
+    }
+    class Marker : Product
+    {
+        private string name;
+        private int sort;
+        public Marker() : base()
+        {
+            this.name = "Фломастерс";
+            this.sort = 1;
+            Input();
+        }
+        public override void ProductCost()
+        {
+            Result = Count * Cost * (1 / Math.Sqrt(sort));
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Кол-во: {Count}\n" +
+                $"Цена: {Cost}\n" +
+                $"Название фломастеров: {name}\n" +
+                $"Сорт: {sort}\n" +
+                $"Новая стоимость: {Result}");
+        }
+        public void Input()
+        {
+            Console.Write("Введите название: ");
+            name = Console.ReadLine();
+            Console.Write("Введите сорт: ");
+            sort = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Mikitchuk_LegacyClasses/Task_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using Task_2;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                A objectMy = new A();
+                Console.WriteLine($"Класс A без параметров\n{objectMy.c}");
+                Console.WriteLine($"Класс A с параметрами");
+                Console.Write($"Введите значение: ");
+                int numOne = int.Parse(Console.ReadLine());
+                Console.Write($"Введите значение: ");
+                int numTwo = int.Parse(Console.ReadLine());
+                A objectMy1 = new A(numOne, numTwo);
+                Console.WriteLine(objectMy1.c);
+                B objectMy2 = new B();
+                Console.WriteLine($"Класс B без параметров\n{objectMy2.c2}");
+                Console.WriteLine($"Класс B с параметрами");
+                Console.Write("Введите действие (сложение-1 | вычитание-2): ");
+                int operation = int.Parse(Console.ReadLine());
+                Console.Write($"Введите значение: ");
+                int numThree = int.Parse(Console.ReadLine());
+                B objectMy3 = new B(operation, numOne, numTwo, numThree);
+                Console.WriteLine(objectMy3.c2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    class A
+    {
+        protected int a;
+        protected int b;
+        public int c
+        {
+            get
+            {
+                if (a > b)
+                {
+                    return a - b;
+                }
+                else
+                {
+                    return b - a;
+                }
+            }
+            set { a = value; b = value; }
+        }
+        public A()
+        {
+            a = 1;
+            b = 1;
+        }
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+    class B : A
+    {
+        private int d;
+        private int operation;
+        public int c2
+        {
+            get
+            {
+                switch (operation)
+                {
+                    case 1:
+                        return a + b + d;
+                    case 2:
+                        return a - b - d;
+                    default:
+                        return 0;
+                }
+            }
+            set
+            { a = value; b = value; d = value; }
+        }
+        public B()
+        {
+            a = 1;
+            b = 1;
+            d = 1;
+        }
+        public B(int operation, int a, int b, int d) : base(a, b)
+        {
+            this.d = d;
+            this.operation = operation;
+        }
+    }
+}
