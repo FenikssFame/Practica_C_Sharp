1afaaadc787c42a0d758ea9b5e16234498f35fc7
Viacheslav Mikitchuk <89093010+FenikssFame@users.noreply.github.com>
2023-05-04 10:11:47 +0300

    04/05/23 created programs

---
+++ Mikitchuk_Patterns/Task/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск Windows");
+            Computer comp = new Computer();
+            comp.Launch("Windows 11");
+            Console.WriteLine(comp.OS.Name);
+            comp.OS = OS.getInstance("Windows 10");
+            Console.WriteLine(comp.OS.Name);
+        }
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.getInstance(osName);
+        }
+    }
+    class OS
+    {
+        private static OS instance;
+        public string Name { get; private set; }
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+                instance = new OS(name);
+            return instance;
+        }
+    }
+}
