2f610ab0f47559b38f087cbc7a8690711f763a52
Viacheslav Mikitchuk <89093010+FenikssFame@users.noreply.github.com>
2023-05-13 19:32:56 +0300

    13/05/23 created programs

---
+++ Mikitchuk_Animations/Task_2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap baner;
+        Rectangle rect;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                baner = new Bitmap(@"..\..\baner.png");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ошибка загрузки файла баннера\n" + e.ToString(), "Баннер");
+                this.Close();
+                return;
+            }
+            g = this.CreateGraphics();
+            rect.X = 0;
+            rect.Y = 0;
+            rect.Width = baner.Width;
+            rect.Height = baner.Height;
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ClearForm();
+            rect.X += 1;
+            if (Math.Abs(rect.X) > rect.Width)
+            {
+                rect.X += rect.Width;
+            }
+            for (int i = 0; i <= Convert.ToInt16(this.ClientSize.Width / rect.Width) + 1; i++)
+            {
+                g.DrawImage(baner, rect.X + i * rect.Width, rect.Y);
+            }
+            if (rect.X == this.Width)
+                rect.X = 0;
+        }
+        private void ClearForm()
+        {
+            g.FillRectangle(new SolidBrush(this.BackColor), new Rectangle(0, 0, this.Width, this.Height));
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Y < rect.Y + rect.Height) && (e.Y > rect.Y))
+            {
+                if (timer1.Enabled != false)
+                {
+                    timer1.Enabled = false;
+                }
+            }
+            else
+            {
+                if (timer1.Enabled != true)
+                {
+                    timer1.Enabled = true;
+                }
+            }
+        }
+    }
+}
---
+++ Mikitchuk_Animations/Task_5/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Task_5
+{
+    public partial class Form1 : Form
+    {
+        int y = 530;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Graphics g1 = e.Graphics;
+            Graphics g2 = e.Graphics;
+            Graphics g3 = e.Graphics;
+            g.Clear(Color.Blue);
+            SolidBrush peg = new SolidBrush(Color.Red);
+            SolidBrush peg1 = new SolidBrush(Color.Yellow);
+            SolidBrush peg2 = new SolidBrush(Color.Yellow);
+            SolidBrush peg3 = new SolidBrush(Color.Aqua);
+            GraphicsPath gp = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp1 = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp2 = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp3 = new GraphicsPath(FillMode.Winding);
+            gp.AddPolygon(new Point[] { new Point(450, y), new Point(465, y + 100), new Point(485, y + 100), new Point(500, y), new Point(475, y - 30), new Point(450, y) });
+            gp1.AddPolygon(new Point[] { new Point(465, y + 100), new Point(445, y + 120), new Point(445, y + 90), new Point(460, y + 70), new Point(465, y + 100) });
+            gp2.AddPolygon(new Point[] { new Point(485, y + 100), new Point(505, y + 120), new Point(505, y + 90), new Point(490, y + 70), new Point(485, y + 100) });
+            gp3.AddEllipse(465, y + 5, 20, 20);
+            g.FillPath(peg, gp);
+            g1.FillPath(peg1, gp1);
+            g2.FillPath(peg2, gp2);
+            g3.FillPath(peg3, gp3);
+        }
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int H = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            y--;
+            H++;
+            if (H == 680)
+            {
+                timer1.Stop();
+                H = 0;
+                y = 530;
+            }
+            Invalidate();
+        }
+    }
+}
