f80a8f0f08764e09eb547cc21c808b8ce2e101ee
Viacheslav Mikitchuk <89093010+FenikssFame@users.noreply.github.com>
2023-05-13 09:15:07 +0300

    11/05/23 created programs

---
+++ Mikitchuk_PresentationFoundation/Task_1/AboutApp.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutApp.xaml
+    /// </summary>
+    public partial class AboutApp : Window
+    {
+        public AboutApp()
+        {
+            InitializeComponent();
+            buttonClose.Click += ButtonClose_Click;
+        }
+        public AboutApp(string appInfo) : this()
+        {
+            textBlockInfo.Text = appInfo;
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Mikitchuk_PresentationFoundation/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonRun.Click += ButtonRun_Click;
+            buttonClose.Click += ButtonClose_Click;
+            buttonAbout.Click += ButtonAbout_Click;
+            textBoxEnterName.MouseEnter += TBMouse_Enter;
+        }
+        private void ButtonAbout_Click(object sender, RoutedEventArgs e)
+        {
+            string info = $"О программе {this.Title}";
+            AboutApp aboutApp = new AboutApp(info);
+            aboutApp.ShowDialog();
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void TBMouse_Enter(object sender, RoutedEventArgs e)
+        {
+            textBoxEnterName.Text = "";
+        }
+        private void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            string hello = "Hello";
+            string input = string.Empty;
+            if (string.IsNullOrEmpty(textBoxEnterName.Text) || string.IsNullOrWhiteSpace(textBoxEnterName.Text) || textBoxEnterName.Text == "Введите имя")
+            {
+                input = "World";
+                textBoxEnterName.Text = "Введите имя";
+            }
+            else
+            {
+                input = textBoxEnterName.Text;
+            }
+            textBlockHello.Text = $"{hello} {input}";
+        }
+    }
+}
